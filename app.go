abe5a6afbc7629d8961e7885803a28f6cbd7154f
Ram Bhat <ram@MacBook-Air.local>
2020-05-07 05:18:49 +0530

    fin

--- app.go
+++ app.go
@@ -1,12 +1,37 @@
 package main
 
-const imagewidth = 16
-const imageheight = 16
+import (
+	"fmt"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 
-func getImageList(foldername string) (spriteNames []string) {
-	return
-}
+	gim "github.com/ozankasikci/go-image-merge"
+)
 
 func main() {
+	files, err := ioutil.ReadDir("sprites")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var grids []*gim.Grid
+	fmt.Println("Creating sprite sheet from following files...")
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".png") {
+
+			grid := gim.Grid{ImageFilePath: file.Name()}
+			grids = append(grids, &grid)
+			fmt.Println(file.Name())
+		}
+	}
+
+	rgba, err := gim.New(grids, len(grids), 1).Merge()
+
+	// save the output to jpg or png
+	file, err := os.Create("sheet.png")
+	err = png.Encode(file, rgba)
 }
